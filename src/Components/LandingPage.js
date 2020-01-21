3eaef0cb3eb2f610867edaffd21dcc6e139a1608
Terrance Chang <tchang46343@gmail.com>
2020-01-20 20:38:06 -0800

    Add layout changes

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -20,12 +20,13 @@ export default class HomePage extends React.Component {
           <header className="Title"> Motivate The Mind</header>
           <h1 className="Instruction">Instructions:</h1>
           <p className="InstructionInfo">
-            Motivate The Mind is a mental health game. The user should feel a
-            sense of positivity and a clearier mindset.The game is played by
-            going through a series of slides that are designed to stimulate the
-            brain's mortor cortex. Each slide will have different content that
-            the user will interpret, but the format will always have a picture,
-            inspiring phrase, and self-reflection.
+            Motivate The Mind is a mental health game application. Users after
+            playing the games should leave with a feeling that promotes
+            positivity and clarity in mindset. The game is played by going
+            through a series of slides. Each slide is designed in a way to
+            increase the stimulation of the user's brain chemical receptor of
+            dopamine and serotonin levels. Try out the game, and let us know
+            your feedback!
           </p>
 
           <button className="StartGame" onClick={NextSlide}>
