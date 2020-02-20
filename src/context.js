2ddfe8a6e299a548d68c09f15f2cf94890277c43
Terrance Chang <tchang46343@gmail.com>
2020-02-19 21:33:57 -0800

    Working through getting gameslide to display

--- src/context.js
+++ src/context.js
@@ -5,45 +5,24 @@ export const ApplicationContext = createContext();
 class ApplicationContextProvider extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      email: [],
-      Password: []
     };
   }
 
   componentDidMount() {
-    fetch("http://localhost:8000/users", {
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
+    fetch("http://localhost:8000/users")
       .then(response => response.json())
-      .then(results => {
-        console.log(results);
-        this.setState({
-          users: results
-        });
-      });
-  }
-
-  updateFilter = (email, Password) => {
-    this.setState({
-      filter: { email, Password }
-    });
-  };
-  render() {
-    const { email, Password } = this.state;
-    const value = {
-      email,
-      filter,
-      updateFilter: this.updateFilter
-    };
-    return (
-      <ApplicationContext.Provider value={value}>
-        {this.props.children}
-      </ApplicationContext.Provider>
-    );
-  }
+      .then(response => {
+        console.log(response);
+      })
 }
+
+  
+  render() 
+    return (
+    <div>
+      <p>grjgengn</p>
+    </div>
+    );
+  
 
 //export default ApplicationContextProvider;
