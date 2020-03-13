05d80fde12f475fd7e224424a02d014d0566a171
Terrance Chang <tchang46343@gmail.com>
2020-03-12 22:40:01 -0700

    testing sign in

--- src/Components/LogIn/SignIn.js
+++ src/Components/LogIn/SignIn.js
@@ -2,7 +2,7 @@ import React from "react";
 import TokenService from "../../services/token-services";
 import AuthApiService from "../../services/user-api-service";
 import "./SignIn.css";
-//import context from "../../context";
+import { API_BASE_URL } from "../../config";
 
 class NewAccount extends React.Component {
   //static contextType = context;
@@ -10,11 +10,21 @@ class NewAccount extends React.Component {
     super(props);
     this.state = {
       email: "",
-      Password: ""
-      // this.props.onLoginSucess()
+      Password: "",
+      userId: "",
+      id: []
     };
   }
-
+  componentDidMount() {
+    fetch(`${API_BASE_URL}users`)
+      .then(res => res.json())
+      .then(id => {
+        this.setState({
+          id
+        });
+      })
+      .catch(err => console.log(err));
+  }
   emailChanged(email) {
     this.setState({
       email
@@ -26,6 +36,18 @@ class NewAccount extends React.Component {
       Password
     });
   }
+  generateIdOptions = e => {
+    const userIds = this.state.id;
+    console.log(userIds);
+    return userIds.map(id => {
+      return (
+        <option key={id.id} value={id.name}>
+          {id.id}
+          {id.name}
+        </option>
+      );
+    });
+  };
 
   handleSubmitBasicAuth = e => {
     e.preventDefault();
@@ -48,20 +70,22 @@ class NewAccount extends React.Component {
   };
 
   render() {
+    const idOptions = this.generateIdOptions();
     return (
       <div>
         <header className="Sign-In"> Account Login </header>
 
         <form className="NewUserSetup" onSubmit={this.handleSubmitBasicAuth}>
           <header className="EmailTag"> User Email:</header>
-          <input
+          <select
             className="email"
             required
-            placeholder="meditate@mind.com"
             name="email"
-            value={this.state.email}
+            value={this.state.userId}
             onChange={e => this.emailChanged(e.target.value)}
-          ></input>
+          >
+            {idOptions}
+          </select>
           <header className="PasswordTag"> Password:</header>
           <input
             placeholder="Password"
