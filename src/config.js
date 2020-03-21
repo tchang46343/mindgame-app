67cd6f6be9ea98d70c7fe364aa69f8f052ec9e69
Terrance Chang <tchang46343@gmail.com>
2020-03-21 13:38:15 -0700

    Updating

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 module.exports = {
   PORT: process.env.PORT || 3000,
   //API_ENDPOINT: "http://localhost:8000",
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000",
-  //"https://mindgame-backend.herokuapp.com",
+  API_BASE_URL:
+    process.env.REACT_APP_API_BASE_URL ||
+    "https://mindgame-backend.herokuapp.com",
 
   TOKEN_KEY: "mindgame-app-token"
 };
