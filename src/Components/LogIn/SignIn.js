3dacd5548555225c99fc40221020ea3cee561ca4
Terrance Chang <tchang46343@gmail.com>
2020-03-21 14:20:28 -0700

    Code is Operational

--- src/Components/LogIn/SignIn.js
+++ src/Components/LogIn/SignIn.js
@@ -35,7 +35,7 @@ class NewAccount extends React.Component {
     })
       .then(res => {
         TokenService.saveAuthToken(res.authToken);
-        this.props.history.push("/slide/1");
+        this.props.history.push("/");
       })
       .catch(res => {
         this.setState({ error: res.error });
